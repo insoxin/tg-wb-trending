8e7a22456520589876e6ebf92c65c136cd7b5e9c
Redy <192666378@qq.com>
2021-01-18 02:11:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function sendTgMessage(data) {
   const ranks = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
   const text = data.splice(1,20).map((o, i) => {
     if (o.promotion) {
-      return `💰 [赞助bitcoin:bc1q2lfx6y52p93qwk6y6yhszzfhjdt2anz43xw6ee
+      return `💰:[bitcoin:bc1q2lfx6y52p93qwk6y6yhszzfhjdt2anz43xw6ee
 ](${o.scheme}) ${(o.desc_extr / 10000).toFixed(2)} 万`
     }
     if (ranks[i]) {
