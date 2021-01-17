82668f15c0ca5e1440861d1a6ff4bceb345cf6c9
Redy <192666378@qq.com>
2021-01-18 02:03:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ async function sendTgMessage(data) {
   const ranks = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
   const text = data.splice(1,20).map((o, i) => {
     if (o.promotion) {
-      return `💰 [${o.desc}](${o.scheme}) ${(o.desc_extr / 10000).toFixed(2)} 万`
+      return `💰 [## bitcoin:bc1q2lfx6y52p93qwk6y6yhszzfhjdt2anz43xw6ee
+](${o.scheme}) ${(o.desc_extr / 10000).toFixed(2)} 万`
     }
     if (ranks[i]) {
       return `${ranks[i]} [${o.desc}](${o.scheme}) ${(o.desc_extr / 10000).toFixed(2)} 万`
     }
-    return `🔥 [${o.desc}](${o.scheme}) ${(o.desc_extr / 10000).toFixed(2)} 万`
-  ' 赞助bitcoin:bc1q2lfx6y52p93qwk6y6yhszzfhjdt2anz43xw6ee'})
+    return `🔥 [${o.desc}](${o.scheme}) ${(o.desc_extr / 10000).toFixed(2)} 万`})
   text.unshift(`${dayjs().format('YYYY-MM-DD HH:MM:ss')} 的微博热搜`)
   await bot.telegram.sendMessage(CHANNEL_ID, text.join('\n'), {
     parse_mode: 'Markdown',
